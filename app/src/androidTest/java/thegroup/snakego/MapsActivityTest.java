7e2f07a4d17064ab41cf19bda7ab02a657da7a79
David Langston <davidrlangston@gmail.com>
2016-10-08 04:54:36 -0400

    Minor changes, edits

--- app/src/androidTest/java/thegroup/snakego/MapsActivityTest.java
+++ app/src/androidTest/java/thegroup/snakego/MapsActivityTest.java
@@ -22,34 +22,34 @@ public class MapsActivityTest {
 
     @Test public void onClickSnakeGoIcon1() {
         // given user is playing game
-        String optionsActivityText = "Options";
+        String optionsActivityText = "Resume Game";
 
-        // when user clicks on the snageGo icon button
+        // when user clicks on the snakeGo icon button
         onView(withId(R.id.icon_button)).perform(click());
 
-        // then the user lands on the options page
+        // then the user lands on the options page and Sees "Resume Game" text
         onView(withText(optionsActivityText)).check(matches(notNullValue()));
     }
 
-    @Test public void onClickSnageGoIcon2() {
+    @Test public void onClickSnakeGoIcon2() {
         // given user is playing game
         String optionsActivityText = "High Scores";
 
-        // when user clicks on the snageGo icon button
+        // when user clicks on the snakeGo icon button
         onView(withId(R.id.icon_button)).perform(click());
 
-        // then the user lands on the options page
+        // then the user lands on the options page and sees "High Scores" text
         onView(withText(optionsActivityText)).check(matches(notNullValue()));
     }
 
-    @Test public void onClickSnageGoIcon3() {
+    @Test public void onClickSnakeGoIcon3() {
         // given user is playing game
         String optionsActivityText = "Quit Game";
 
-        // when user clicks on the snageGo icon button
+        // when user clicks on the snakeGo icon button
         onView(withId(R.id.icon_button)).perform(click());
 
-        // then the user lands on the options page
+        // then the user lands on the options page and sees "Quit Game" text
         onView(withText(optionsActivityText)).check(matches(notNullValue()));
     }
 
