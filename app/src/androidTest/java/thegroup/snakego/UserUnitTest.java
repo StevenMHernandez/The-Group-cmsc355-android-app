223f83627fe2489be662a5e2a75cf652b2482da7
relfpmik <kimpflerze@vcu.edu>
2016-10-11 20:16:12 -0400

    Added some tests for the snake and for the User class.

--- app/src/androidTest/java/thegroup/snakego/UserUnitTest.java
+++ app/src/androidTest/java/thegroup/snakego/UserUnitTest.java
@@ -2,6 +2,8 @@ package thegroup.snakego;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 import thegroup.snakego.Models.User;
@@ -9,11 +11,13 @@ import thegroup.snakego.Models.User;
 import static org.junit.Assert.*;
 
 /**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ * This test class is for the MapDisplay user story.
  */
+
 public class UserUnitTest {
     @Test
     public void testSingleton() throws Exception {
+        //Assert that the created singletons have the same values at start
         User userOne = User.get();
         User userTwo = User.get();
 
@@ -21,7 +25,18 @@ public class UserUnitTest {
     }
 
     @Test
+    public void userAtStart() {
+        //Assert that the length & score & highscore of the user("Snake") is zero at start
+        Assert.assertEquals(0, User.get().getSnake().size());
+
+        Assert.assertEquals(0, User.get().getScore());
+
+        Assert.assertEquals(0, User.get().getHighScore());
+    }
+
+    @Test
     public void testSnakeLength() throws Exception {
+        //Assert that the snake isnt longer than it should be depending on the score
         User user = User.get();
         for(int i = 0; i<100; ++i){
             user.onLocationUpdated(new LatLng(37.0, 77.0));
@@ -30,5 +45,26 @@ public class UserUnitTest {
         }
 
     }
+
+    @Test
+    public void testAddPointToScore() {
+        User.get().addPoints(1);
+        Assert.assertEquals(1, User.get().getScore());
+    }
+
+    @Test
+    public void testRemovePointFromScore(){
+        User.get().addPoints(1);
+        User.get().removePoints(1);
+        Assert.assertEquals(0, User.get().getScore());
+    }
+
+    @Test
+    public void testAddPointToSnake(){
+        User.get().onLocationUpdated(new LatLng(33.0, -77.0));
+        Assert.assertEquals(1, User.get().getSnake().size());
+    }
+
+
 
 }
